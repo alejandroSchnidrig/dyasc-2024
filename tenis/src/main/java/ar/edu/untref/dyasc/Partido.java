5e9478d80da0951c792e6ee2850a1cd7354248bc
Fede <weber.federic@gmail.com>
2024-09-27 19:15:41 -0300

    Partido puede finalizarse

--- tenis/src/main/java/ar/edu/untref/dyasc/Partido.java
+++ tenis/src/main/java/ar/edu/untref/dyasc/Partido.java
@@ -1,6 +1,8 @@
 package ar.edu.untref.dyasc;
 
 public class Partido {
+
+	private PuntajeHandler  puntajeHandler = new PuntajeHandler();
 	
 	private final String MENSAJE_PARTIDO_TERMINADO  = "Ganador del partido: ";
 	
@@ -9,8 +11,9 @@ public class Partido {
 	}
 	
 	public String jugarPartido(Jugador jugador1, Jugador jugador2) {
-		while(jugador1.getSets() == 3 || jugador2.getSets() == 3) {
+		while(jugador1.getSets() < 3 && jugador2.getSets() < 3) {
 			jugarPunto(jugador1, jugador2);
+			puntajeHandler.procesarPuntaje(jugador1,jugador2);
 		}
 		
 		return jugador1.getSets() == 3 ? MENSAJE_PARTIDO_TERMINADO + jugador1.getNombre() : MENSAJE_PARTIDO_TERMINADO + jugador2.getNombre();
--- tenis/src/test/java/ar/edu/untref/dyasc/PartidoTest.java
+++ tenis/src/test/java/ar/edu/untref/dyasc/PartidoTest.java
@@ -33,5 +33,11 @@ public class PartidoTest {
 		Jugador jugador1 = new Jugador("Jugador1");
 		Jugador jugador2 = new Jugador("Jugador2");
 		partido.jugarPartido(jugador1, jugador2);
+		boolean ganoPartido = false;
+		if(jugador1.getSets() == 3  || jugador2.getSets() == 3 ) {
+			ganoPartido = true;
+		}
+		assertThat(ganoPartido).isEqualTo(true);
+
 	}
 }
