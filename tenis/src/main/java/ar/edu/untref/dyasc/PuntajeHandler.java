ce8e637d5028c5b46a2ef79db28a30e6e51e9b54
Fede <weber.federic@gmail.com>
2024-09-28 12:48:25 -0300

    El partido puede entrar en Tie Brake y gana set Jugador1

--- tenis/src/main/java/ar/edu/untref/dyasc/PuntajeHandler.java
+++ tenis/src/main/java/ar/edu/untref/dyasc/PuntajeHandler.java
@@ -3,6 +3,15 @@ package ar.edu.untref.dyasc;
 public class PuntajeHandler {
 
     public void procesarPuntaje(Jugador jugador1, Jugador jugador2, Estados estadoPartido) {
+        if(Estados.TIE_BRAKE == estadoPartido) {
+            if(jugador1.getPuntaje() - jugador2.getPuntaje() >= 2 ) {
+                jugador1.ganoSet();
+                jugador1.resetearPuntaje();
+                jugador1.resetearGame();
+                jugador2.resetearPuntaje();
+                jugador2.resetearGame();
+            }
+        }
     	if(Estados.DEUCE == estadoPartido) {
     		if(jugador1.getPuntaje() - jugador2.getPuntaje() >= 2 && jugador1.getGames() < 5) {
     			jugador1.ganoGame();
